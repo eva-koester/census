9a2b8ba8c9fb7506edf28916e181a0b2506a8a9e
Eva <eva.maria.koester@gmx.de>
2019-01-28 21:46:21 +0100

    within census_2.db, create the a census table and read in the census csv file

---
+++ read_census_into_db_2.py
@@ -0,0 +1,23 @@
+# sqlite3: create the new table 'census_2' in census_2.db and read in an existing table (census)
+import sqlite3
+import csv
+
+# connect to the database 'census_2.db'
+conn = sqlite3.connect('census_2.db')
+cursor = conn.cursor()
+
+# create the table census_2 (should be run once and then commented out)
+cursor.execute(
+    '''CREATE TABLE census_2
+         (state INT NOT NULL,
+         sex TEXT NOT NULL,
+         age INT NOT NULL,
+         pop2000 INT NOT NULL,
+         pop2008 INT NOT NULL);''')
+
+csvReader = csv.reader(open('census.csv'), delimiter=',', quotechar='"')
+for row in csvReader:
+        conn.execute('insert into census_2 (state, sex, age, pop2000, pop2008) values (?, ?, ?, ?, ?)', row)
+
+cursor.execute('SELECT * FROM census_2')
+print(cursor.fetchall())
