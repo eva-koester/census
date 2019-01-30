fa89e92238b6ef270d7acb4967b30e35b82eac5f
Eva <eva.maria.koester@gmx.de>
2019-01-30 14:33:27 +0100

    count no of countries with life exp. above avg in 2010

---
+++ life_exp_above_avg.py
@@ -0,0 +1,14 @@
+# countries that had high average life expectancies (at the country level) in 2015.
+import sqlite3
+conn = sqlite3.connect('census_2.db')
+cursor = conn.cursor()
+
+# determine average life expectancy in 2010:
+cursor.execute('SELECT AVG(life_expectancy) FROM population WHERE year = 2010')
+
+# select everything from countries with a life expectancy greater than 1.15*average (64.78) in 2010:
+cursor.execute('SELECT COUNT(*) FROM population WHERE life_expectancy > 1.15 * ('
+               'SELECT AVG(life_expectancy) FROM population WHERE year = 2010) AND year = 2010')
+
+print(cursor.fetchall())
+
